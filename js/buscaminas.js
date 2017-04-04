4b56669067252659782d80eb9d3eedc632c7f94a
Edith Mandujano <edithmandujano001@gmail.com>
2017-04-04 18:53:45 -0500

    con boton jugar de nuevo

--- js/buscaminas.js
+++ js/buscaminas.js
@@ -1,28 +1,39 @@
 var minas =document.getElementsByClassName('mina');
 var numeros= document.getElementsByClassName('num');
 var vacio=document.getElementsByClassName('vacio');
+var jugar= document.getElementById('jugar');
 console.log (minas);
+jugar.addEventListener("click", recargar);
+function recargar(){
+  location.reload()
+}
+
 //este for recoorre mi arreglo donde están los botones con classe mina y les porne la funcion onclick
 for (var i= 0;i< minas.length;i ++){
   minas[i].addEventListener("click", perdiste);
-  function perdiste(){
-    this.className="minaPresionada";
-    alert("game over");
-  }
-}
 
+}
+function perdiste(){
+  this.className="minaPresionada";
+  alert("game over")
+  //button.removeEventListener("click");
+
+}
 
 
 //este for recoorre mi arreglo donde están los botones con classenumeros y les porne la funcion onclick
 for (var i= 0;i< numeros.length;i ++){
   numeros[i].addEventListener("click", cambiar);
-  function cambiar(){
-    this.className="numer";
-    console.log (numeros);
-  }
+
 
 }
 
+function cambiar(){
+  this.className="numer";
+  var numero=this.value;
+  this.innerHTML=numero;
+  console.log (numeros);
+}
 //este for recoorre mi arreglo donde están los botones con classevacio y les porne la funcion onclick
 for (var i= 0;i< vacio.length;i ++){
   vacio[i].addEventListener("click", cambiarClase);
@@ -32,7 +43,3 @@ for (var i= 0;i< vacio.length;i ++){
   }
 
 }
-
-/*for (var i= 0;i< vacio.length;i ++){
-  vacio[i].addEventListener("click", cambiar);
-}*/
